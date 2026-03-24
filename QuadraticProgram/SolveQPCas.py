97313b1344415681d086406652fe71e5c85cd837
DreVDB <dre.vandenbroeke@gmail.com>
2026-03-24 15:01:27 +0100

    ok

--- QuadraticProgram/SolveQPCas.py
+++ QuadraticProgram/SolveQPCas.py
@@ -1,41 +1,51 @@
 import casadi as ca
 import numpy as np
 
-def SolveQPCas(Q, c, A, b, Aeq, beq):
-    n = Q.shape[0]
+import casadi as ca
+import numpy as np
 
-    # CasADi QP datastructuur
+def SolveQPCas(Q, c, A, b, Aeq, beq):
+    """
+    Lost een QP op:
+        minimize 0.5 xᵀ Q x + cᵀ x
+        subject to A x ≤ b
+                   Aeq x = beq
+    met CasADi's QP solver.
+    """
+
+    Q = np.array(Q, dtype=float)
+    c = np.array(c, dtype=float)
+    A = np.array(A, dtype=float)
+    b = np.array(b, dtype=float)
+    Aeq = np.array(Aeq, dtype=float)
+    beq = np.array(beq, dtype=float)
+
+    n = Q.shape[0]
+    m = A.shape[0]
+    k = Aeq.shape[0]
+
+    # QP datastructuur
     qp = {}
 
-    # 0.5 xᵀ Q x + cᵀ x
-    qp['H'] = ca.DM(Q)
-    qp['g'] = ca.DM(c)
+    qp["H"] = ca.DM(Q)
+    qp["g"] = ca.DM(c)
 
-    # Combineer A en Aeq
-    A_total = A if Aeq is None else np.vstack([A, Aeq])
-    qp['A'] = ca.DM(A_total)
+    # Combineer ongelijkheid + gelijkheid constraints
+    A_total = np.vstack([A, Aeq])
+    qp["A"] = ca.DM(A_total)
 
-    # Lower en upper bounds voor constraints
-    m = A.shape[0]
+    # Bounds maken
+    lba = np.concatenate([-np.inf * np.ones(m), beq])
+    uba = np.concatenate([b, beq])
 
-    # Ongelijkheden: A x ≤ b  ->  laten we lba = -inf, uba = b
-    lba = -ca.inf*m
-    uba = b
+    qp["lba"] = ca.DM(lba)
+    qp["uba"] = ca.DM(uba)
 
-    # Gelijkheden: Aeq x = beq -> lba = uba = beq
-    if Aeq is not None:
-        k = Aeq.shape[0]
-        lba = np.concatenate([lba, beq])
-        uba = np.concatenate([uba, beq])
+    # Variable bounds (onbegrensd)
+    qp["lbx"] = -ca.inf * np.ones(n)
+    qp["ubx"] =  ca.inf * np.ones(n)
 
-    qp['lba'] = ca.DM(lba)
-    qp['uba'] = ca.DM(uba)
-
-    # Variabelen onbegrensd
-    qp['lbx'] = -ca.inf*n
-    qp['ubx'] = ca.inf*n
-
-    # QP oplossen
+    # Los het QP op
     sol = ca.qp(qp)
 
-    return np.array(sol['x']).reshape(-1)
+    return np.array(sol["x"]).reshape(-1)
