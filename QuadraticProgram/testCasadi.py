783dc1704451e528d99177f839068fb56561af9a
Alexander Persyn <alexander.persyn@student.kuleuven.be>
2026-03-24 16:35:58 +0100

    a

--- QuadraticProgram/testCasadi.py
+++
@@ -1,9 +0,0 @@
-import casadi as ca
-
-x = ca.SX.sym('x')
-y = ca.SX.sym('y')
-qp = {'x': ca.vertcat(x, y), 'f': x**2 + y**2, 'g': x + y - 10}
-
-S = ca.qpsol('S', 'qpoases', qp)
-r = S(lbg=0)
-print(r['x'])
