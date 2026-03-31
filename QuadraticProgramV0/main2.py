b84a6b444eeafc9e66519329ca5834933aab05df
Alexander Persyn <alexander.persyn@student.kuleuven.be>
2026-03-31 15:31:24 +0200

    a

--- QuadraticProgramV0/main2.py
+++ QuadraticProgramV0/main2.py
@@ -109,9 +109,9 @@ def train_warm_start_model(X, y, n, m, k, epochs=12, batch_size=64):
 
 
 def main(k=1):
-    samples = 100
+    samples = 200
     n = 100
-    m = 50
+    m = 10
     epochs = 15
     batch_size = 64
     seed = 7
