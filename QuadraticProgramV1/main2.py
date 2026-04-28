7b2a0e6188ff7f4507255590d82f09f5f6d8fa54
kasperuytterhoeven <kasper.uytterhoeven@student.kuleuven.be>
2026-04-28 13:56:30 +0200

    optimalisatielayers weg

--- QuadraticProgramV1/main2.py
+++ QuadraticProgramV1/main2.py
@@ -161,8 +161,8 @@ def train_warm_start_model(X, y, n, m, k, epochs=12, batch_size=64):
 
 def main(k=1):
     samples = 100
-    n = 200  
-    m = 150
+    n = 20
+    m = 15
     epochs = 15
     batch_size = 64
     seed = 7
